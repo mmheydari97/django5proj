df19a48afb0a21cea14ecd84acacf374a62f9862
mmheydari97 <heidarymm@yahoo.com>
2024-10-05 21:20:47 -0400

    chapter 05

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from .models import Profile
 
 
@@ -12,7 +12,7 @@ class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'first_name', 'email']
     
     def clean_password2(self):
@@ -21,10 +21,26 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+    
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['first_name', 'last_name', 'email']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        
+        return data
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
